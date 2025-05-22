6f8e620c49d529155b9df6ae5c544402ab49bc06
xodls <pider6@naver.com>
2025-05-22 17:49:03 +0900

    유저 urls연결설정

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/books/', include('books.urls')),
+    path('api/users/', include('users.urls')),
 
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
